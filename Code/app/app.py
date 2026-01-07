bf77c39d8492935a649ccbddf7ad9b4ed0d63a8d
Om Thakur <live.onkar@gmail.com>
2026-01-07 05:44:28 -0500

    app.py

--- Code/app/app.py
+++ Code/app/app.py
@@ -1,23 +1,36 @@
 import streamlit as st
+# on stream lit
 from pipeline.pipeline import AnimeRecommendationPipeline
 from dotenv import load_dotenv
-
+# mandatory for st to provide the page config
 st.set_page_config(page_title="Anime Recommnder",layout="wide")
 
 load_dotenv()
 
 @st.cache_resource
+# to initialize the pipeline
+
+# What it does:
+# Caches the return value of the function
+# Runs the function only on the first call
+# Reuses the cached result on subsequent calls
+# Persists across user interactions and reruns
 def init_pipeline():
     return AnimeRecommendationPipeline()
 
 pipeline = init_pipeline()
 
 st.title("Anime Recommender System")
+# now we give the query , a place user can ask question in the stremlit
 
 query = st.text_input("Enter your anime prefernces eg. : light hearted anime with school settings")
+# if query provided 
 if query:
+    # fetching recommendations for you.....
+    # spinner is used to show the user that the system is working
+    # make it better looking
     with st.spinner("Fetching recommendations for you....."):
-        response = pipeline.recommend(query)
+        response = pipeline.recommend(query)# output is generated
         st.markdown("### Recommendations")
         st.write(response)
 
