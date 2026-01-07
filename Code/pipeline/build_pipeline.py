bc864bddefcf01aee87e902445fdde04de42f011
Om Thakur <live.onkar@gmail.com>
2026-01-07 04:55:08 -0500

    pipeline building , one is vector store builder , other is recommender

--- Code/pipeline/build_pipeline.py
+++ Code/pipeline/build_pipeline.py
@@ -13,20 +13,25 @@ def main():
         logger.info("Starting to build pipeline...")
 
         loader = AnimeDataLoader("data/anime_with_synopsis.csv" , "data/anime_updated.csv")
+        # anime upadted data is after processing the data
         processed_csv = loader.load_and_process()
-
+# store the processed.csv
         logger.info("Data  loaded and processed...")
-
+# build the vector store
         vector_builder = VectorStoreBuilder(processed_csv)
         vector_builder.build_and_save_vectorstore()
-
+# vector store is built sucesfully
+# as it is not returning any value , we don't need to store it in a variable
         logger.info("Vector store Built sucesfully....")
 
-        logger.info("Pipelien built sucesfuly....")
+        logger.info("Pipelien built sucesfully....")
     except Exception as e:
             logger.error(f"Failed to execute pipeline {str(e)}")
             raise CustomException("Error during pipeline " , e)
     
 if __name__=="__main__":
      main()
+# creating a vector store using the build_pipeline.py file
+# pipeline.py is loading the vectors form the store 
+# but here we generating those vectors
 
--- Code/pipeline/pipeline.py
+++ Code/pipeline/pipeline.py
@@ -1,15 +1,22 @@
 from src.vector_store import VectorStoreBuilder
+# this is the vector store builder class that we have created in the vector_store.py file
 from src.recommender import AnimeRecommender
+# this is the recommender class that we have created in the recommender.py file
 from config.config import GROQ_API_KEY,MODEL_NAME
 from utils.logger import get_logger
+# this is the logger class that we have created in the logger.py file
 from utils.custom_exception import CustomException
+# this is the custom exception class that we have created in the custom_exception.py file
 
 logger = get_logger(__name__)
+# logger is initialized here
 
 class AnimeRecommendationPipeline:
     def __init__(self,persist_dir="chroma_db"):
+        # uses the persist_dir , where the vector data store is 
         try:
             logger.info("Intializing Recommdation Pipeline")
+            # track custom exceptions
 
             vector_builder = VectorStoreBuilder(csv_path="" , persist_dir=persist_dir)
 
@@ -34,6 +41,7 @@ class AnimeRecommendationPipeline:
         except Exception as e:
             logger.error(f"Failed to get recommendation {str(e)}")
             raise CustomException("Error during getting recommendation" , e)
+            # this pipeline is used for generating the responses
         
 
 
